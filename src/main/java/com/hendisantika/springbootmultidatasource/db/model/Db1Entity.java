cba06b0df9beb03c6224b0f4214553e6d75bb4bb
Hendi Santika <hendisantika@gmail.com>
2020-01-06 19:52:02 +0700

    #5 - Add Db1Entity

---
+++ src/main/java/com/hendisantika/springbootmultidatasource/db/model/Db1Entity.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootmultidatasource.db.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multidatasource
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/01/20
+ * Time: 19.51
+ */
+@Entity
+@Data
+public class Db1Entity {
+    @Id
+    Integer key;
+
+    @Column(name = "database_name")
+    String databaseName;
+
+}
