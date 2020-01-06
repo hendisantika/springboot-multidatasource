02d83f0ee1963265c368647f663b5e43369aea7e
Hendi Santika <hendisantika@gmail.com>
2020-01-06 20:00:10 +0700

    #17 - Add entityManagerFactory in Db2Config

--- src/main/java/com/hendisantika/springbootmultidatasource/config/Db2Config.java
+++ src/main/java/com/hendisantika/springbootmultidatasource/config/Db2Config.java
@@ -4,9 +4,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -37,6 +39,16 @@ public class Db2Config {
     @ConfigurationProperties("db2.datasource.configuration")
     public DataSource dataSource(@Qualifier("dataSourceProperties") DataSourceProperties dataSourceProperties) {
         return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("dataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.gokuldasputhenpurakkal.db2.model")
+                .persistenceUnit("db2")
                 .build();
     }
 }
