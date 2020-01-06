b0c1f688d0aa5e95009d3c302ba8216bb50aa0de
Hendi Santika <hendisantika@gmail.com>
2020-01-06 20:01:18 +0700

    #20 - Add testDatabase1 API in DbController

--- src/main/java/com/hendisantika/springbootmultidatasource/controller/DbController.java
+++ src/main/java/com/hendisantika/springbootmultidatasource/controller/DbController.java
@@ -1,8 +1,13 @@
 package com.hendisantika.springbootmultidatasource.controller;
 
+import com.hendisantika.springbootmultidatasource.db1.model.Db1Entity;
 import com.hendisantika.springbootmultidatasource.db1.repository.Db1EntityRepository;
 import com.hendisantika.springbootmultidatasource.db2.repository.Db2EntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,5 +27,10 @@ public class DbController {
 
     @Autowired
     Db2EntityRepository db2EntityRepository;
+
+    @GetMapping(value = "/api/db1", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Db1Entity testDatabase1(@RequestHeader(required = true, value = HttpHeaders.ACCEPT_LANGUAGE) final String languageCode) {
+        return db1EntityRepository.getDb2Entity();
+    }
 
 }
