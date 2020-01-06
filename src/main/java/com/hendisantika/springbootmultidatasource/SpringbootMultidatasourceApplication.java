e70f24498fb2e9f92552d7daf5be6deb817b7726
Hendi Santika <hendisantika@gmail.com>
2020-01-06 19:46:25 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmultidatasource/SpringbootMultidatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultidatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultidatasourceApplication.class, args);
+    }
+
+}
