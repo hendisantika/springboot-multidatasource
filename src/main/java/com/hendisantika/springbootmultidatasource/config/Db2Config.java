48326d0283964704f921d601fb84efe3b29a2337
Hendi Santika <hendisantika@gmail.com>
2020-01-06 19:59:50 +0700

    #16 - Add dataSource in Db2Config

--- src/main/java/com/hendisantika/springbootmultidatasource/config/Db2Config.java
+++ src/main/java/com/hendisantika/springbootmultidatasource/config/Db2Config.java
@@ -1,11 +1,15 @@
 package com.hendisantika.springbootmultidatasource.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +31,12 @@ public class Db2Config {
     @ConfigurationProperties("db2.datasource")
     public DataSourceProperties dataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @Bean(name = "dataSource")
+    @ConfigurationProperties("db2.datasource.configuration")
+    public DataSource dataSource(@Qualifier("dataSourceProperties") DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class)
+                .build();
     }
 }
