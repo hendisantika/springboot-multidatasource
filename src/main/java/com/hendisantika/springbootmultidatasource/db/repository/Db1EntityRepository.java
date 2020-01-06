1f47b04a7f08d8826171fe589e14ba6b5cb01f3f
Hendi Santika <hendisantika@gmail.com>
2020-01-06 19:52:39 +0700

    #6 - Add Db1EntityRepository

---
+++ src/main/java/com/hendisantika/springbootmultidatasource/db/repository/Db1EntityRepository.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootmultidatasource.db.repository;
+
+import com.hendisantika.springbootmultidatasource.db.model.Db1Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multidatasource
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/01/20
+ * Time: 19.52
+ */
+public interface Db1EntityRepository extends JpaRepository<Db1Entity, Long> {
+
+    @Query(value = "SELECT 1 AS key, current_database() AS database_name ", nativeQuery = true)
+    Db1Entity getDb2Entity();
+
+}
